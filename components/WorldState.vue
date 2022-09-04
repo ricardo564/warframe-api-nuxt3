162eff9c7e5e9e1bef4ae8027f9b141244aabddf
Ricardo Camilo <39412058+ricardo564@users.noreply.github.com>
2022-09-04 18:14:51 -0300

    feat: create WorldState component

---
+++ components/WorldState.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const { data: news, pending } = await useFetch("/api/worldState");
+</script>
+<template>
+  <p v-if="pending">carregando....</p>
+  <div class="h-auto max-w-screen h-[99.9vh] w-full" md="mx-auto ">
+    <WorldStateNews class="max-h-[10vh]" :newsData="news" />
+  </div>
+</template>
