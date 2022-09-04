73cfd9720ce1224bfba89a009e81352d88d87511
Ricardo Camilo <39412058+ricardo564@users.noreply.github.com>
2022-09-04 18:12:14 -0300

    feat: create Figure component

---
+++ components/tag/Figure.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { defineProps } from "@vue/runtime-core";
+
+const props = defineProps({
+  figureStyle: {
+    type: String,
+    default: " w-full h-full ",
+  },
+  captionStyle: {
+    type: String,
+    default: " text-center ",
+  },
+  caption: {
+    type: String,
+    default: null,
+  },
+});
+</script>
+<template>
+  <figure v-bind="$attrs" :class="figureStyle">
+    <slot />
+    <figcaption v-if="caption" :class="captionStyle">
+      {{ caption }}
+    </figcaption>
+  </figure>
+</template>
