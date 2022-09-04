feb3782ed6cf5c24c10625c5fb2fea416c7440cf
Ricardo Camilo <39412058+ricardo564@users.noreply.github.com>
2022-09-04 18:11:57 -0300

    feat: create Img component

---
+++ components/tag/Img.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { defineProps } from "@vue/runtime-core";
+
+const props = defineProps({
+  src: {
+    type: String,
+    default: null,
+  },
+  alt: {
+    type: String,
+    default: null,
+  },
+  customStyle: {
+    type: String,
+    default: " w-full h-full ",
+  },
+});
+</script>
+<template>
+  <img :src="src" :alt="alt" :class="customStyle" />
+</template>
