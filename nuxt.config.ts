c2ede4a62870c1cf75a39da79efc87aa9e1667e0
Ricardo Camilo <39412058+ricardo564@users.noreply.github.com>
2022-09-04 20:00:52 -0300

    build: install @kevinmarrec/nuxt-pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,17 @@ export default defineNuxtConfig({
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
-  buildModules: ["nuxt-windicss"],
+  buildModules: ["nuxt-windicss", "@kevinmarrec/nuxt-pwa"],
+  pwa: {
+    manifest: {
+      name: "Warframe-api Nuxt3 Windicss",
+      short_name: "WNW",
+      theme_color: "#000000",
+    },
+    workbox: {
+      enabled: true,
+    },
+  },
   runtimeConfig: {
     public: {
       BASE_URL: process.env.NUXT_PUBLIC_BASE_URL,
